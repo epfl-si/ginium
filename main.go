b8404ca499c51b9ff4b5d74d4cf0acc4b4097c2a
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-02-01 10:44:52 +0100

    [security] Fix a warning about trusted proxies
    [Tell Gin to trust no one](https://pkg.go.dev/github.com/gin-gonic/gin#Engine.SetTrustedProxies)

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ func main() {
     // Initialize the Gin router
     router := gin.Default()
 
+    // No X-Forwarded-For business (yet)
+    router.SetTrustedProxies(nil)
+
     // Define a route handler for the root path
     router.GET("/", func(c *gin.Context) {
         c.JSON(200, gin.H{

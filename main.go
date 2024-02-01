3093cf5a0ae5ce2c08dae8380489d8390671bd41
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-02-01 18:20:52 +0100

    [feature] OIDC access control, “Web 2.0”-style
    - Add the missing `/api/v1/private` GET route to go with the `<button>` in `index.html`
    - Use dud OAuth2 state, because we don't want to plaster “this application uses cookies etc.”, if we don't have to
    - Protect it with a `TokenAuthMiddleware`, itself fashioned out of an `*oidc.Provider` in which we use the `UserInfo` method (given that JavaScript, not Golang, obtained the ID token)

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
+    "context"
     "net/http"
+    "strings"
 
+    "golang.org/x/oauth2"
+    "github.com/coreos/go-oidc/v3/oidc"
     "github.com/gin-gonic/gin"
 )
+
+func TokenAuthMiddleware(provider *oidc.Provider) gin.HandlerFunc {
+    return func(c *gin.Context) {
+        // Get the bearer token from the request header
+        tokenString := c.GetHeader("Authorization")
+        if tokenString == "" {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+            c.Abort()
+            return
+        }
+
+        // Parse and verify the token
+        bearerToken := strings.Split(tokenString, "Bearer ")[1]
+
+        _, err := provider.UserInfo(context.Background(), oauth2.StaticTokenSource(&oauth2.Token{AccessToken: bearerToken}))
+	if err != nil {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Failed to get userinfo: "+err.Error()})
+            c.Abort()
+	    return
+	}
+
+        // Token is valid, continue with the request
+        c.Next()
+    }
+}
 
 func main() {
     // Initialize the Gin router
@@ -23,6 +52,33 @@ func main() {
     router.GET("/api/v1/public", func(c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "Hello, Gin!",
+        })
+    })
+
+    // Create an OpenID Connect provider
+    provider, err := oidc.NewProvider(context.Background(), "http://localhost:8080/realms/golang")
+    if err != nil {
+        panic(err)
+    }
+
+    clientID := "hello_golang"
+
+    router.GET("/login", func(c *gin.Context) {
+	config := oauth2.Config{
+		ClientID:     clientID,
+		Endpoint:     provider.Endpoint(),
+		RedirectURL:  "http://localhost:8088/",
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+
+        nonce := "samesamebutdifferent"
+        c.Redirect(http.StatusFound, config.AuthCodeURL(nonce, oidc.Nonce(nonce)))
+    })
+
+    // Define a route handler that is protected by OpenID-Connect
+    router.GET("/api/v1/private", TokenAuthMiddleware(provider), func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "I is serious cat. This is serious data.",
         })
     })
 
